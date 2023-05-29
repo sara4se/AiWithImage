d80abfbef6efacdd56718d6479780635caf85378
sara <sara.ahumidi4@gmail.com>
2023-05-29 18:19:03 +0300

    Initial Commit

---
+++ AItTestImageTWQ/AItTestImageTWQApp.swift
@@ -0,0 +1,17 @@
+//
+//  AItTestImageTWQApp.swift
+//  AItTestImageTWQ
+//
+//  Created by Sara Alhumidi on 09/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AItTestImageTWQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AItTestImageTWQ/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AItTestImageTWQ
+//
+//  Created by Sara Alhumidi on 09/11/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
